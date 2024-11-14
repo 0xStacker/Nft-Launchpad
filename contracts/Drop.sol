0069ae68c8210ae02c25567399f0b58ee168823b
0xStacker <oladotuntemitopeakorede@gmail.com>
2024-11-15 00:45:18 +0100

    update

--- contracts/Drop.sol
+++ contracts/Drop.sol
@@ -3,28 +3,29 @@ pragma solidity 0.8.25;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 contract Drop is ERC721{
-    uint immutable MAX_SUPPLY;
-    uint price;
-    address immutable owner;
-    address immutable creator;
-    uint tokenId;
-    uint startTime;
-    uint royalty;
-    bool immutable dynamicPricing;
+    uint public immutable MAX_SUPPLY;
+    uint public totalMinted;
+    uint public price;
+    address public immutable owner;
+    address public immutable creator;
+    uint private tokenId;
+    uint private startTime;
+    uint private royalty;
+    uint mintFee;
+    // bool internal immutable dynamicPricing;
     bool isSoulbound;
 
 
     constructor(string memory _name, string memory _symbol,
-     uint _maxSupply, uint _startTime, bool _isSoulbound,
-      address _owner, uint _royalty,
-       address _creator,
-       bool _dynamicPricing) ERC721(_name, _symbol){
+     uint _maxSupply, uint _startTime,
+      address _owner,
+       address _creator, uint _mintFee) ERC721(_name, _symbol){
         creator = _creator;
         MAX_SUPPLY = _maxSupply;
         startTime = _startTime;
-        isSoulbound = _isSoulbound;
-        dynamicPricing = _dynamicPricing;
+        // dynamicPricing = _dynamicPricing;
         owner = _owner;
+        mintFee = _mintFee;
     }
 
     modifier onlyOwner{
@@ -32,23 +33,63 @@ contract Drop is ERC721{
         _;
     }
 
-    error InsufficientFunds(uint _cost);
+    modifier onSale{
+        require(block.timestamp >= startTime);
+        _;
+    }
 
-    function mintPublic(uint _amount, address _to) external payable{
-        if(msg.value < price){
-            revert InsufficientFunds(price);
+    error InsufficientFunds(uint _cost);
+    error SupplyExceeded(uint maxSupply);
+
+    function _getCost(uint amount) internal view returns (uint cost){
+        
+        return (price * amount) + mintFee;
+    }
+
+    function _mintNft(address _to) internal {
+        tokenId += 1;
+        totalMinted += 1;
+        _safeMint(_to, tokenId);
+    }
+
+
+    function mintPublic(uint _amount, address _to) onSale external payable{
+        uint totalCost = _getCost(_amount);
+        if(msg.value < totalCost){
+            revert InsufficientFunds(totalCost);
         }
+        if (totalMinted + _amount > MAX_SUPPLY){
+            revert SupplyExceeded(MAX_SUPPLY);
+            }
+
         for(uint i; i < _amount; i++){
-            tokenId += 1;
-            _safeMint(_to, tokenId);
+            _mintNft(_to);
         }
         
     }
 
-    function airdrop(address _to) external onlyOwner{
-        tokenId += 1;
-        _safeMint(_to, tokenId);
+    function airdrop(address _to, uint _amount) external onlyOwner{
+        if (totalMinted + _amount > MAX_SUPPLY){
+            revert SupplyExceeded(MAX_SUPPLY);
+        }
+        for(uint i; i < _amount; i++){
+            _mintNft(_to);
+        }
     }
+
+    function batchAirdrop(address[] calldata _receipients, uint _amountPerAddress) external onlyOwner{
+        if (totalMinted + (_amountPerAddress * _receipients.length) > MAX_SUPPLY){
+            revert SupplyExceeded(MAX_SUPPLY);
+        }
+        for(uint i; i < _receipients.length; i++){
+            for(uint j; j < _amountPerAddress; j++){
+                _mintNft(_receipients[i]);
+            }
+        }
+    }
+
+
+
 
     function whitelistMint() external {}
 
