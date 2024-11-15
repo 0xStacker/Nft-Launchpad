842656672744d15f191030a0c676a8bf62901f48
0xStacker <oladotuntemitopeakorede@gmail.com>
2024-11-15 12:10:29 +0100

    update

--- contracts/Drop.sol
+++ contracts/Drop.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/MerkleProof.sol";
+
 
 contract Drop is ERC721{
     uint public immutable MAX_SUPPLY;
@@ -14,16 +16,18 @@ contract Drop is ERC721{
     uint mintFee;
     bool public paused;
     bool isSoulbound;
-
+    uint maxPerWallet;
+    using MerkleProof for bytes32[];
 
     constructor(string memory _name, string memory _symbol,
      uint _maxSupply, uint _startTime,
       address _owner,
-       address _creator, uint _mintFee) ERC721(_name, _symbol){
+       address _creator, uint _mintFee, uint _maxPerWallet) ERC721(_name, _symbol){
         creator = _creator;
         MAX_SUPPLY = _maxSupply;
         startTime = _startTime;
         // dynamicPricing = _dynamicPricing;
+        maxPerWallet = _maxPerWallet;
         owner = _owner;
         mintFee = _mintFee;
     }
@@ -43,6 +47,14 @@ contract Drop is ERC721{
         _;
     }
 
+    function _canMint(uint _amount) internal view returns (bool){
+        if (totalMinted + _amount > MAX_SUPPLY){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
     error InsufficientFunds(uint _cost);
     error SupplyExceeded(uint maxSupply);
 
@@ -51,55 +63,79 @@ contract Drop is ERC721{
         return (price * amount) + mintFee;
     }
 
-    function _mintNft(address _to) internal {
-        tokenId += 1;
-        totalMinted += 1;
-        _safeMint(_to, tokenId);
+    function _mintNft(address _to, uint _amount) internal {
+        for(uint i; i < _amount; i++){
+            tokenId += 1;
+            totalMinted += 1;
+            _safeMint(_to, tokenId);
+        }
+
     }
 
-
-    function mintPublic(uint _amount, address _to) external saleStarted isPaused payable{
+    function mintPublic(uint _amount) external saleStarted isPaused payable{
+        if (!_canMint(_amount)){
+            revert SupplyExceeded(MAX_SUPPLY);
+        }
 
         uint totalCost = _getCost(_amount);
         if(msg.value < totalCost){
             revert InsufficientFunds(totalCost);
         }
-        if (totalMinted + _amount > MAX_SUPPLY){
-            revert SupplyExceeded(MAX_SUPPLY);
-            }
-
-        for(uint i; i < _amount; i++){
-            _mintNft(_to);
-        }
-        
+        _mintNft(msg.sender, _amount);
     }
 
+    function controlledMint(uint _amount) external saleStarted isPaused payable{
+        uint amountMintable = _amount;
+        if (!_canMint(_amount)){
+            uint amountLeft = (MAX_SUPPLY - totalMinted);
+            if(amountLeft >= maxPerWallet){
+                amountMintable = maxPerWallet;
+            } else{
+                amountMintable = amountLeft;
+            }
+        }
+        uint totalCost = _getCost(amountMintable);
+        if(msg.value < totalCost){
+            revert InsufficientFunds(totalCost);
+        }
+        _mintNft(msg.sender, amountMintable);
+    }
+
+
     function airdrop(address _to, uint _amount) external onlyOwner{
-        if (totalMinted + _amount > MAX_SUPPLY){
+        if(!_canMint(_amount)){
             revert SupplyExceeded(MAX_SUPPLY);
         }
-        for(uint i; i < _amount; i++){
-            _mintNft(_to);
-        }
+        _mintNft(_to, _amount);
     }
 
     function batchAirdrop(address[] calldata _receipients, uint _amountPerAddress) external onlyOwner{
-        if (totalMinted + (_amountPerAddress * _receipients.length) > MAX_SUPPLY){
+        uint totalAmount = _amountPerAddress * _receipients.length;
+        if (!_canMint(totalAmount)){
             revert SupplyExceeded(MAX_SUPPLY);
         }
         for(uint i; i < _receipients.length; i++){
-            for(uint j; j < _amountPerAddress; j++){
-                _mintNft(_receipients[i]);
-            }
+            _mintNft(_receipients[i], _amountPerAddress);
         }
-
     }
 
     function pauseSale() external saleStarted onlyOwner{
         paused = true;
     }
 
+    error NotWhitelisted(address _address);
 
-    function whitelistMint() external {}
+
+    function whitelistMint(bytes32[] memory _proof, bytes32 _root, bytes32 _leaf, uint _amount) external saleStarted isPaused{
+        if (!_canMint(_amount)){
+            revert SupplyExceeded(MAX_SUPPLY);
+        }
+
+        bool whitelisted = _proof.verify(_root, _leaf);
+        if(!whitelisted){
+            revert NotWhitelisted(msg.sender);
+        }
+        _mintNft(msg.sender, _amount);
+    }
 
 } 
