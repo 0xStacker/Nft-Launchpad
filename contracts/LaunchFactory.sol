76e60d86539559cfe847e2d2513a8ba137750d73
0xStacker <oladotuntemitopeakorede@gmail.com>
2024-11-17 21:39:11 +0100

    update

---
+++ contracts/LaunchFactory.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+import "./Drop.sol";
+
+contract LaunchFactory{
+    uint mintFee = 200000000000000;
+    
+    function createDrop(string memory _name, string memory _symbol,
+     uint _maxSupply, uint _startTime, uint _price, uint _maxPerWallet) external returns(address){
+        Drop newDrop = new Drop(_name, _symbol, _maxSupply, _startTime,msg.sender, msg.sender,
+        mintFee, _price, _maxPerWallet);
+        return address(newDrop);
+       }
+}
