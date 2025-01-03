64de55540dc7242c59ccd9ab65ab6a969c8d8ac2
0xStacker <oladotuntemitopeakorede@gmail.com>
2025-01-03 08:12:13 +0100

    update

--- tests/Drop_test.sol
+++ tests/Drop_test.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
         
 pragma solidity >=0.4.22 <0.9.0;
 
 // This import is automatically injected by Remix
 import "remix_tests.sol"; 
-
+import {PresaleLib} from "../contracts/Drop.sol";
 // This import is required to use custom transaction context
 // Although it may fail compilation in 'Solidity Compiler' plugin
 // But it will work fine in 'Solidity Unit Testing' plugin
@@ -12,7 +12,7 @@ import "remix_accounts.sol";
 import {Drop} from "../contracts/Drop.sol";
 
 // File name has to end with '_test.sol', this file can contain more than one testSuite contracts
-contract testSuite {
+contract testSuite{
     Drop public newDrop;
     address acc0;
     address acc1;
@@ -20,17 +20,43 @@ contract testSuite {
     address drop;
     uint public mintedCount;
 
+    /*
+        uint8 maxPerAddress;
+        string name;
+        uint price;
+        uint startTime;
+        uint endTime;
+        bytes32 merkleRoot;
+    */
+    
+    PresaleLib.PresalePhase[] phases;
     /// 'beforeAll' runs before all other tests
     /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
-    /// #sender: account-0
     function beforeAll() public payable{
         acc0 = TestsAccounts.getAccount(0); 
         acc1 = TestsAccounts.getAccount(1);
         acc2 = TestsAccounts.getAccount(2);
+        PresaleLib.PresalePhase memory newPhase = phases.push();
+        newPhase.maxPerAddress = 2;
+        newPhase.name = "testPhase";
+        newPhase.price = 50;
+        newPhase.startTime = 0;
+        newPhase.endTime = 200;
+        newPhase.merkleRoot = 0x85c99f9ed408529a8e32d19f1606c0783273722f7a42ae71ef5f7345b0e62870;
+        phases.push(newPhase);
+        Drop _newDrop = new Drop("TEST",
+         "TST",
+         10,
+         0,
+         300,
+         acc0,
+         0,
+         100,
+         2,
+         true,
+         phases
+         );
 
-        Drop _newDrop = new Drop("TEST", "TST",
-     21, 0, acc0,
-       acc0, 0, 100, 2);
        newDrop = _newDrop;
        drop = address(_newDrop);
        Assert.equal(acc0, newDrop.creatorAddress(), "Owner doesn't match");
@@ -47,30 +73,12 @@ contract testSuite {
         Assert.equal(newDrop.creatorAddress().balance, creatorBalance + 200, "Creator Balance should increase by 200");
     }
 
-    /// #sender: account-2
-    /// #value: 200
-    function testControlledMint() public payable{
-        uint creatorBalance = newDrop.creatorAddress().balance;
-        uint currentBal = newDrop.balanceOf(msg.sender);
-        newDrop.controlledMint{value: 200}(5, msg.sender);
-        Assert.equal(newDrop.balanceOf(msg.sender), currentBal + 2, "Balance should increase by 2");
-        Assert.equal(newDrop.creatorAddress().balance, creatorBalance + 200, "Creator Balance should increase by 200");
-    }
-
     /// #sender: account-1
     function testAirdrop() external{
         uint initialBal = newDrop.balanceOf(acc1);
         newDrop.airdrop(acc1, 2);
         Assert.equal(newDrop.balanceOf(acc1), initialBal + 2, "Airdrop should transfer 2 tokens from account->account-3");
-
     }
 
-    /// # sender: account-3
-    /// #value: 100
-    function testMintAfterResume() public payable{
-        uint initialBal = newDrop.balanceOf(msg.sender);
-        newDrop.mintPublic{value: 100}(1, msg.sender); 
-        Assert.equal(newDrop.balanceOf(msg.sender), initialBal + 1, "Balance should increase by 1");
-    }
 }
     
