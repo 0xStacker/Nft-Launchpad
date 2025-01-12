beea336a165454cbbdde968e002c84b930f705a6
0xStacker <oladotuntemitopeakorede@gmail.com>
2025-01-12 15:46:15 +0100

    update factory

--- contracts/LaunchFactory.sol
+++ contracts/LaunchFactory.sol
@@ -1,27 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 import {Drop} from "./Drop.sol";
-import {PresaleLib} from "./PresaleLib.sol";
 
 contract LaunchFactory{
     uint mintFee = 200000000000000;
-    address[] public dropList;
-    mapping(address => address[] _drops) public userDrops;
+    Drop[] public dropList;
+    mapping(address => Drop[] _drops) public userDrops;
 
-    /*
-        string memory _name,
-    string memory _symbol,
-    uint _maxSupply,
-    uint _startTime,
-    uint _duration,
-    address _owner,   
-    uint _mintFee,
-    uint _price,
-    uint8 _maxPerWallet,
-    bool _includePresale,
-    PresaleLib.PresalePhase[] memory _presalePhases   
-    */
-    
     function createERC721Drop(string memory _name,
     string memory _symbol,
     uint _maxSupply,
@@ -30,7 +15,7 @@ contract LaunchFactory{
     address _owner,
     uint _price,
     uint8 _maxPerWallet
-    ) external returns(address){
+    ) external{
         Drop newDrop = new Drop(_name,
         _symbol,
         _maxSupply,
@@ -40,15 +25,13 @@ contract LaunchFactory{
         mintFee,
         _price,
         _maxPerWallet);
-        dropList.push(address(newDrop));
-        userDrops[_owner].push(address(newDrop));
-        return address(newDrop);
-       }
-       
-    
-    function getDropsByCreator(address _creator) external view returns(address[] memory){
+        dropList.push(newDrop);
+        userDrops[_owner].push(newDrop);
+       }       
+   
+
+    function getDropsByCreator(address _creator) external view returns(Drop[] memory){
         return userDrops[_creator];
     }
-    
-    
+        
 }
